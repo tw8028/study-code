206d55b971b02e1f2b237d05ae35b81c0efa4603
tengwei <bcy0btw@163.com>
2024-08-07 19:45:48 +0800

    2024-8-7

---
+++ cSharp/ServerApp2/ServerApp2/CategoryConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerApp2;
+
+public record CategoryConfig
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Key { get; set; }
+    public string? SourceRoot { get; set; }
+    public string? DestRoot { get; set; }
+}
