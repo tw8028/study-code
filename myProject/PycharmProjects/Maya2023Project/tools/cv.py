6e170eff5805283487391d29c4ea5a07ee20ce5b
tengwei <bcy0btw@163.com>
2025-03-11 20:40:53 +0800

    311

--- myProject/PycharmProjects/Maya2023Project/tools/cv.py
+++ myProject/PycharmProjects/Maya2023Project/tools/cv.py
@@ -2,7 +2,9 @@ import pymel.core as pm
 
 
 def _create(name, radius, points):
-    return pm.curve(n=name, d=1, p=[tuple(float(radius * x) for x in i) for i in points], k=list(range(len(points))))
+    knot = list(range(len(points)))
+    point = [tuple([radius * x for x in i]) for i in points]
+    return pm.curve(n=name, d=1, p=point, k=knot)
 
 
 def cube(name='cube', radius=10):
@@ -12,7 +14,13 @@ def cube(name='cube', radius=10):
 
 
 def ball(name='boll', radius=1):
-    pass
+    points = [(-0.0, 0.0, 1.0), (-0.7, -0.0, 0.7), (-1.0, 0.0, 0.0), (-0.7, 0.0, -0.7), (0.0, 0.0, -1.0),
+              (0.7, 0.0, -0.7), (1.0, -0.0, -0.0), (0.7, -0.0, 0.7), (-0.0, -0.0, 1.0), (0.0, -0.7, 0.7),
+              (-0.0, -1.0, -0.0), (0.0, -0.7, -0.7), (0.0, 0.0, -1.0), (-0.0, 0.7, -0.7), (0.0, 1.0, 0.0),
+              (-0.0, 0.7, 0.7), (-0.0, -0.0, 1.0), (0.0, -0.7, 0.7), (0.0, -1.0, 0.0), (0.7, -0.7, -0.0),
+              (1.0, -0.0, 0.0), (0.7, 0.7, 0.0), (-0.0, 1.0, -0.0), (-0.7, 0.7, 0.0), (-1.0, 0.0, -0.0),
+              (-0.7, -0.7, -0.0), (0.0, -1.0, 0.0)]
+    return _create(name, radius, points)
 
 
 def rhombus(name='rhombus', radius=10):
