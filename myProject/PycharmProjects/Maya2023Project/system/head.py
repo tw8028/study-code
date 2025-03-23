1a2d2ce049784a2f6054e831f785945edf01c73c
bcy0btw@163.com <bcy0btw@163.com>
2025-03-23 22:12:49 +0800

    head 修改

--- myProject/PycharmProjects/Maya2023Project/system/head.py
+++ myProject/PycharmProjects/Maya2023Project/system/head.py
@@ -9,10 +9,12 @@ class Head:
     # name: jnt__c__head__001
     def __init__(self, *, joints: list[str]):
         self.joints = joints
-        self.root = f'root__c__neck__001'
+        self.joint_root = f'root__c__neck__001'
         self.joints_fk = [f'jnt__c__{i}_fk__001' for i in joints]
-        self.neck_fk = self.joints_fk[0]
-        self.head_fk = self.joints_fk[-1]
+        self.joint_neck_fk = self.joints_fk[0]
+        self.joint_head_fk = self.joints_fk[-1]
+        self.joint_no_roll_01 = 'jnt__c__neck_no_roll_001'
+        self.joint_no_roll_02 = 'jnt__c__neck_no_roll_002'
 
         self.ctrl_neck = f'ctrl__c__neck__001'
         self.zero_neck = f'zero__c__neck__001'
@@ -23,24 +25,34 @@ class Head:
 
     def create(self):
         # create fk joints
-        mytools.jnt_target(name=self.root, target=self.joints[0])
+        mytools.jnt_target(name=self.joint_root, target=self.joints[0])
         for jnt, jnt_fk in zip(self.joints, self.joints_fk):
             mytools.jnt_target(name=jnt_fk, target=jnt)
-        mytools.parent_chain([self.root] + self.joints_fk)
+        mytools.parent_chain([self.joint_root] + self.joints_fk)
 
         # create neck controller
-        mytools.cv_target(name=self.ctrl_neck, target=self.neck_fk, shape='cube', radius=4)
+        mytools.cv_target(name=self.ctrl_neck, target=self.joint_neck_fk, shape='cube', radius=4)
         mytools.grp_zero(name=self.zero_neck, target=self.ctrl_neck)
         # create head controller
-        mytools.cv_target(name=self.ctrl_head, target=self.head_fk, shape='cube', radius=4)
+        mytools.cv_target(name=self.ctrl_head, target=self.joint_head_fk, shape='cube', radius=4)
         mytools.grp_zero(name=self.zero_head, target=self.ctrl_head)
         mytools.grp_zero(name=self.drive_head, target=self.zero_head)
 
+        # create no roll joint
+        mytools.jnt_target(name=self.joint_no_roll_01, target=self.joints[0])
+        mytools.jnt_target(name=self.joint_no_roll_02, target=self.joints[-1])
+        pm.parent(self.joint_no_roll_02, self.joint_no_roll_01)
+        pm.parent(self.joint_no_roll_01, self.joint_root)
+        handle = pm.ikHandle(name=f'handle__c__neck__001', startJoint=self.joint_no_roll_01,
+                            endEffector=self.joint_no_roll_02)[0]
+        pm.poleVectorConstraint(self.joint_no_roll_01, handle)
+        pm.parent(handle, self.ctrl_neck)
+
     def rig(self):
         input_obj_nd = pm.PyNode(self.zero_neck)
-        target_obj_nd = pm.PyNode(self.ctrl_neck)
-        root_nd = pm.PyNode(self.root)
-        name = self.root.split('__', 1)[1]
+        target_obj_nd = pm.PyNode(self.joint_no_roll_01)
+        root_nd = pm.PyNode(self.joint_root)
+        name = self.joint_root.split('__', 1)[1]
         blend_matrix_nd = pm.createNode('blendMatrix', name='blendM__' + name)
         blend_matrix_nd.envelope.set(1 / len(self.joints))
         mult_matrix_nd = pm.createNode('multMatrix', name='multM__' + name)
@@ -52,8 +64,9 @@ class Head:
             jnt_fk_nd = pm.PyNode(jnt)
             var = mult_matrix_nd.matrixSum >> jnt_fk_nd.offsetParentMatrix  # type:ignore
 
-        pm.pointConstraint(self.head_fk, self.drive_head)
-        pm.orientConstraint(self.ctrl_head, self.head_fk)
+        output_ctrl_neck = mytools.grp_sub(name=f'output_{self.ctrl_neck}', target=self.ctrl_neck)
+        pm.matchTransform(output_ctrl_neck, self.joint_head_fk, position=True)
+        pm.pointConstraint(output_ctrl_neck, self.drive_head)
 
     def constraint_deform_joint(self):
         for jnt, jnt_fk in zip(self.joints, self.joints_fk):
