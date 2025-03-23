fc1b1977f172eefa428ed52732cad030912864cd
bcy0btw@163.com <bcy0btw@163.com>
2025-03-23 17:35:35 +0800

    修改head

--- myProject/PycharmProjects/Maya2023Project/system/head.py
+++ myProject/PycharmProjects/Maya2023Project/system/head.py
@@ -9,7 +9,10 @@ class Head:
     # name: jnt__c__head__001
     def __init__(self, *, joints: list[str]):
         self.joints = joints
-        self.cvs_fk = [f'cv__c__{i}_fk__001' for i in joints]
+        self.cog = f'cog__c__neck__001'
+        self.joints_fk = [f'jnt__c__{i}_fk__001' for i in joints]
+        self.neck_fk = self.joints_fk[0]
+        self.head_fk = self.joints_fk[-1]
 
         self.ctrl_neck = f'ctrl__c__neck__001'
         self.zero_neck = f'zero__c__neck__001'
@@ -20,32 +23,33 @@ class Head:
 
     def create(self):
         # create fk joints
-        for jnt, jnt_fk in zip(self.joints, self.cvs_fk):
-            mytools.cv_target(name=jnt_fk, target=jnt, shape='ball', radius=2)
-        mytools.parent_chain(self.cvs_fk)
-        cv_neck_fk = self.cvs_fk[0]
-        cv_head_fk = self.cvs_fk[-1]
+        mytools.jnt_target(name=self.cog, target=self.joints[0])
+        for jnt, jnt_fk in zip(self.joints, self.joints_fk):
+            mytools.jnt_target(name=jnt_fk, target=jnt)
+        mytools.parent_chain([self.cog] + self.joints_fk)
+        jnt_neck_fk = self.joints_fk[0]
+        jnt_head_fk = self.joints_fk[-1]
 
         # create neck controller
-        mytools.cv_target(name=self.ctrl_neck, target=cv_neck_fk, shape='cube', radius=4)
+        mytools.cv_target(name=self.ctrl_neck, target=jnt_neck_fk, shape='cube', radius=4)
         mytools.grp_zero(name=self.zero_neck, target=self.ctrl_neck)
         # create head controller
-        mytools.cv_target(name=self.ctrl_head, target=cv_head_fk, shape='cube', radius=4)
+        mytools.cv_target(name=self.ctrl_head, target=jnt_head_fk, shape='cube', radius=4)
         mytools.grp_zero(name=self.zero_head, target=self.ctrl_head)
         mytools.grp_zero(name=self.drive_head, target=self.zero_head)
 
         # rig
         decompose_nd = mytools.blend_matrix(input=self.zero_neck, target=self.ctrl_neck,
                                             blend_attr=1 / len(self.joints))
-        for cv in self.cvs_fk:
-            cv_nd = pm.PyNode(cv)
-            var = decompose_nd.outputRotate >> cv_nd.rotate  # type:ignore
-        pm.pointConstraint(cv_head_fk, self.drive_head)
+        for jnt in self.joints_fk:
+            jnt_nd = pm.PyNode(jnt)
+            # var = decompose_nd.outputRotate >> jnt_nd.rotate  # type:ignore
+        pm.pointConstraint(jnt_head_fk, self.drive_head)
 
     def constraint_deform_joint(self):
         pass
 
 
 if __name__ == '__main__':
-    head = Head(joints=['joint1', 'joint2', 'joint3', 'joint4'])
+    head = Head(joints=['neck_01', 'neck_02', 'head'])
     head.create()
