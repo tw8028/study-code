8499e5e2a34b2658c3bee5e9570f2b0184167405
tengwei <bcy0btw@163.com>
2025-03-26 11:39:30 +0800

    setup

---
+++ myProject/PycharmProjects/Maya2023Project/setup.py
@@ -0,0 +1,9 @@
+import pymel.core as pm
+import reload
+import custom_shelf.buttons
+
+if not pm.commandPort(":4434", query=True):
+    pm.commandPort(name=":4434")
+
+reload.main()
+custom_shelf.buttons.rebuild()
