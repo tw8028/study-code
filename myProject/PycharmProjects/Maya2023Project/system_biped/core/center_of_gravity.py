a1df8fe099c5757630f5760704c36b3a7c9b59e5
tengwei <bcy0btw@163.com>
2025-04-03 10:58:21 +0800

    修改 blend

--- myProject/PycharmProjects/Maya2023Project/system_biped/core/center_of_gravity.py
+++ myProject/PycharmProjects/Maya2023Project/system_biped/core/center_of_gravity.py
@@ -1,7 +1,6 @@
 import pymel.core as pm
 import mytools
 from system_biped.core.master import Master
-from system_biped.interface.joint_limb import IJoint_limb
 
 
 class CenterOfGravity(object):
@@ -19,11 +18,6 @@ class CenterOfGravity(object):
 
         self._create()
         self._constraint_bones()
-
-        self.ctrl_attr = f'ctrl__{self.side}__{self.name}_ikfk__001'
-        self.zero_attr = f'zero__{self.side}__{self.name}_ikfk__001'
-        self.attr_blend = f'{self.ctrl_attr}.ikFkBlend'
-        self.reverse_node = f'reverse__{self.side}__{self.name}__001'
 
     def _create(self):
         pm.group(name=self.grp_rig, empty=True)
@@ -47,40 +41,6 @@ class CenterOfGravity(object):
 
         if pm.objExists(Master.ctrl_root):
             pm.parent(self.grp_rig, Master.ctrl_root)
-
-    @staticmethod
-    def _create_connect_point(parent, position, connection_type, side):
-        # side 表示被链接组件的边
-        name = f'connect__{side}__{connection_type.value}__001'
-        if pm.objExists(name):
-            return pm.PyNode(name)
-        return mytools.grp_child(name=name, parent=parent, position=position)
-
-    def create_ikfk_attr(self):
-        mytools.cv_and_zero(name=self.ctrl_attr, target=self.ctrl_cog, shape='cross1', radius=2)
-        mytools.zero_orient(self.zero_attr)
-        mytools.lock_hide_transform(self.ctrl_attr)
-        pm.parent(self.zero_attr, self.grp_rig)
-        pm.addAttr(self.ctrl_attr, longName='ikFkBlend', attributeType='float', minValue=0, maxValue=1, dv=1,
-                   keyable=True)
-        reverse_nd = pm.createNode('reverse', name=self.reverse_node)
-        pm.PyNode(self.attr_blend) >> reverse_nd.inputX  # type: ignore
-
-    def blend_jnt(self, fk_system: IJoint_limb, ik_system: IJoint_limb):
-        attr_blend = self.attr_blend
-        reverse_nd = self.reverse_node
-
-        joints_ik = ik_system.get_rig_joints()
-        joints_fk = fk_system.get_rig_joints()
-
-        mytools.blend_orient(attr_ctrl=attr_blend, reverse=reverse_nd, ik_jnt=joints_ik[0], fk_jnt=joints_fk[0],
-                             blend_jnt=self.joints[0])
-        mytools.blend_orient(attr_ctrl=attr_blend, reverse=reverse_nd, ik_jnt=joints_ik[1], fk_jnt=joints_fk[1],
-                             blend_jnt=self.joints[1])
-        mytools.blend_orient(attr_ctrl=attr_blend, reverse=reverse_nd, ik_jnt=joints_ik[2], fk_jnt=joints_fk[2],
-                             blend_jnt=self.joints[2])
-        mytools.blend_scale_x(attr_ctrl=attr_blend, ik_jnt=joints_ik[0], fk_jnt=joints_fk[0], blend_jnt=self.joints[0])
-        mytools.blend_scale_x(attr_ctrl=attr_blend, ik_jnt=joints_ik[1], fk_jnt=joints_fk[1], blend_jnt=self.joints[1])
 
 
 if __name__ == '__main__':
--- myProject/PycharmProjects/Maya2023Project/system_biped/limb.py
+++ myProject/PycharmProjects/Maya2023Project/system_biped/limb.py
@@ -8,6 +8,7 @@ from system_biped.core.center_of_gravity import CenterOfGravity
 from system_biped.core.fk_system import FkSystem
 from system_biped.core.ik_system import IkSystem
 from system_biped.core.mid_system import MidSystem
+from system_biped.interface.joint_limb import IJoint_limb
 
 
 class Limb(IConnectionPointProvider, IConnectionPointUser, ABC):
@@ -16,21 +17,54 @@ class Limb(IConnectionPointProvider, IConnectionPointUser, ABC):
         fk = FkSystem(cog=self.limb)
         ik = IkSystem(cog=self.limb)
         mid = MidSystem(ik=ik)
-        self.limb.create_ikfk_attr()
-        self.limb.blend_jnt(fk_system=fk, ik_system=mid)
 
         self.side = side
         self.connect_point = f'connect__{side}__{name}__001'
         self._create_connect_point()
+
+        self.ctrl_attr = f'ctrl__{side}__{name}_ikfk__001'
+        self.zero_attr = f'zero__{side}__{name}_ikfk__001'
+        self.attr_blend = f'{self.ctrl_attr}.ikFkBlend'
+        self.reverse_node = f'reverse__{side}__{name}__001'
+        self._create_ikfk_attr()
+        self._blend_jnt(fk_system=fk, ik_system=mid)
+
+    def _create_ikfk_attr(self):
+        mytools.cv_and_zero(name=self.ctrl_attr, target=self.limb.ctrl_cog, shape='cross1', radius=2)
+        mytools.zero_orient(self.zero_attr)
+        mytools.lock_hide_transform(self.ctrl_attr)
+        pm.parent(self.zero_attr, self.limb.grp_rig)
+        pm.addAttr(self.ctrl_attr, longName='ikFkBlend', attributeType='float', minValue=0, maxValue=1, dv=1,
+                   keyable=True)
+        reverse_nd = pm.createNode('reverse', name=self.reverse_node)
+        pm.PyNode(self.attr_blend) >> reverse_nd.inputX  # type: ignore
+
+    def _blend_jnt(self, fk_system: IJoint_limb, ik_system: IJoint_limb):
+        attr_blend = self.attr_blend
+        reverse_nd = self.reverse_node
+
+        joints_ik = ik_system.get_rig_joints()
+        joints_fk = fk_system.get_rig_joints()
+
+        mytools.blend_orient(attr_ctrl=attr_blend, reverse=reverse_nd, ik_jnt=joints_ik[0], fk_jnt=joints_fk[0],
+                             blend_jnt=self.limb.joints[0])
+        mytools.blend_orient(attr_ctrl=attr_blend, reverse=reverse_nd, ik_jnt=joints_ik[1], fk_jnt=joints_fk[1],
+                             blend_jnt=self.limb.joints[1])
+        mytools.blend_orient(attr_ctrl=attr_blend, reverse=reverse_nd, ik_jnt=joints_ik[2], fk_jnt=joints_fk[2],
+                             blend_jnt=self.limb.joints[2])
+        mytools.blend_scale_x(attr_ctrl=attr_blend, ik_jnt=joints_ik[0], fk_jnt=joints_fk[0],
+                              blend_jnt=self.limb.joints[0])
+        mytools.blend_scale_x(attr_ctrl=attr_blend, ik_jnt=joints_ik[1], fk_jnt=joints_fk[1],
+                              blend_jnt=self.limb.joints[1])
 
     def _create_connect_point(self):
         jnt_end = self.limb.joints[-1]
         mytools.grp_sub(name=self.connect_point, target=jnt_end)
 
     def get_connection_point(self, connection_type: ConnectionType, side=''):
-        if connection_type.value == ConnectionType.WRIST:
+        if connection_type.value == ConnectionType.WRIST.value:
             return self.connect_point
-        elif connection_type.value == ConnectionType.FOOT:
+        elif connection_type.value == ConnectionType.FOOT.value:
             pass
 
     def connect_to(self, point_provider: IConnectionPointProvider, connection_type: ConnectionType):
