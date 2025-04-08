bea7d385c6b02eafad75006d7b91dfd41677541c
tengwei <bcy0btw@163.com>
2025-04-08 14:27:06 +0800

    add data

--- myProject/CSharp/ImportAssetsToUnity/ImportAssetsToUnity/Infrastructure/AssetsFactory.cs
+++ myProject/CSharp/ImportAssetsToUnity/ImportAssetsToUnity/Infrastructure/AssetsFactory.cs
@@ -14,13 +14,35 @@ public static class AssetsFactory
             DestDirectory: "D:/GitHub/NLDClient/ProjectNLD/Assets/Art/Animations/Battle/Skill");
         IAssets skill = new AssetsNoFolder(skillConfig);
 
-        Category motionConfig = new(Name: "motion",
+        Category motionConfig = new(
+            Name: "motion",
             SearchPattern: "*.fbx",
             SourceDirectory: "E:/project_A/03_动作/ani_motion/_fbx",
             DestDirectory: "D:/GitHub/NLDClient/ProjectNLD/Assets/Art/Animations/Battle/Motion");
         IAssets motion = new AssetsNoFolder(motionConfig);
+        
+        Category playerConfig = new(
+            Name: "player",
+            SearchPattern: "*.fbx",
+            SourceDirectory: "E:/project_A/03_动作/skin_character/_fbx",
+            DestDirectory: "D:/GitHub/NLDClient/ProjectNLD/Assets/Art/Character/Models/Players");
+        IAssets player = new AssetsPlayer(playerConfig);
+        
+        Category bagConfig = new(
+            Name: "bag",
+            SearchPattern: "*.fbx",
+            SourceDirectory: "E:/project_A/03_动作/skin_item/_fbx",
+            DestDirectory: "D:/GitHub/NLDClient/ProjectNLD/Assets/Art/Character/Models/Bag");
+        IAssets bag = new AssetsSingle(bagConfig);
+        
+        Category gunConfig = new(
+            Name: "gun",
+            SearchPattern: "*.fbx",
+            SourceDirectory: "E:/project_A/03_动作/skin_item/_fbx",
+            DestDirectory: "D:/GitHub/NLDClient/ProjectNLD/Assets/Art/Character/Models/Guns");
+        IAssets gun = new AssetsSingle(gunConfig);
 
 
-        return [skill, motion];
+        return [skill, motion, player, bag, gun];
     }
 }
