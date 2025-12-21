37ac25f96dda5c42215ada0e32b7c2e650494687
tengwei <bcy0btw@163.com>
2025-12-21 14:07:12 +0800

    userSetup

--- myProject/PycharmProjects/Maya2023Project/userSetup.py
+++ myProject/PycharmProjects/Maya2023Project/userSetup.py
@@ -1,9 +1,17 @@
-# maya.env  PYTHONPATH = D:\github\study-code\PycharmProjects\mayaProject
+# maya.env
+# PYTHONPATH=D:\GitHub\study-code\myProject\PycharmProjects\Maya2023Project;
 # file path = C:\Users\Administrator\Documents\maya\2023
 
 # userSetup.py
 # file path = C:\Users\Administrator\Documents\maya\2023\scripts
 # 注意语言，中文版文件位置不一样
+# 改英文 resources/110n/en_US
+
+'''
+pycharm项目设置
+1. Python Interpreter：maya安装目录 "D:\Program Files\Autodesk\Maya2023\bin\mayapy.exe"
+2. Project Structure: 添加项目位置 "D:\GitHub\study-code\myProject\PycharmProjects\Maya2023Project"
+'''
 
 import maya.cmds as cmds
 
